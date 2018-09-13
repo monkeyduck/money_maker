2009bb1af0cca02b397d28cdc3836262274097c8
linchuanli <linchuanli@creditease.cn>
2018-09-13 17:29:47 +0800

    1. 增加etc运行版本（run.py） 2. 增加买卖操作后发送邮件 3. 增加批量下单避免量大无法成交 4. 卖出操作前增加撤单功能，撤掉买入没有完全成交的订单 5. 增加analyse回测功能和测试功能

---
+++ analyse.py
@@ -0,0 +1,208 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+import codecs
+import json
+import datetime
+import time
+from trade import okSpot
+
+
+class ticker:
+    def __init__(self, json):
+        self.channel = json['channel']
+        self.data = json['data']
+        for json_data in json.loads(self.data):
+            self.transactions.append()
+
+
+buy_amount = [0.0, 0.0, 0.0]
+sell_amount = [0.0, 0.0, 0.0]
+buy_count = [0, 0, 0]
+sell_count = [0, 0, 0]
+
+
+class dealEntity:
+    def __init__(self, _id, _price, _amount, _time, _type):
+        self.id = _id
+        self.price = _price
+        self.amount = _amount
+        self.time = _time
+        self.type = _type
+
+    def detail(self):
+        if self.type == 'ask':
+            category = 'sell '
+        else:
+            category = 'buy  '
+        return self.time + ': ' + category + self.amount + '\t at price: ' + self.price
+
+    def filter_high(self):
+        if float(self.amount) > 2000:
+            if self.type == 'ask':
+                sell_count[0] += 1
+                sell_amount[0] += float(self.amount)
+            else:
+                buy_count[0] += 1
+                buy_amount[0] += float(self.amount)
+        elif float(self.amount) > 500:
+            if self.type == 'ask':
+                sell_count[1] += 1
+                sell_amount[1] += float(self.amount)
+            else:
+                buy_count[1] += 1
+                buy_amount[1] += float(self.amount)
+        elif float(self.amount) < 100:
+            if self.type == 'ask':
+                sell_count[2] += 1
+                sell_amount[2] += float(self.amount)
+            else:
+                buy_count[2] += 1
+                buy_amount[2] += float(self.amount)
+
+
+class depthEntity:
+    def __init__(self, json):
+        self.asks = json['asks']
+        self.bids = json['bids']
+        self.timestamp = json['timestamp']
+
+    def print_detail(self):
+        print (timestamp2string(self.timestamp))
+        for i in range(0, 20):
+            print ('sell' + str(20 - i) + '\t' + self.asks[i][0] + ':\t' + self.asks[i][1])
+        for i in range(0, 20):
+            print ('buy ' + str(i + 1) + '\t' + self.bids[i][0] + ':\t' + self.bids[i][1])
+
+    def cal_ratio(self):
+        bid = 0.0
+        ask = 0.0
+        bid10 = 0.0
+        ask10 = 0.0
+        bid5 = 0.0
+        ask5 = 0.0
+        for i in range(0, 20):
+            if i < 10:
+                ask10 += float(self.asks[19 - i][1])
+                bid10 += float(self.bids[i][1])
+            if i < 5:
+                ask5 += float(self.asks[19 - i][1])
+                bid5 += float(self.bids[i][1])
+            bid += float(self.bids[i][1])
+            ask += float(self.asks[i][1])
+        ratio20 = (bid - ask) / (bid + ask) * 100
+        ratio10 = (bid10 - ask10) / (bid10 + ask10) * 100
+        ratio5 = (bid5 - ask5) / (bid5 + ask5) * 100
+        print ('5: ' + str(ratio5))
+        print ('10: ' + str(ratio10))
+        print ('20: ' + str(ratio20))
+
+
+class tickerEntity:
+    def __init__(self, json):
+        self.high = json['high']
+        self.low = json['low']
+        self.vol = json['vol']
+        self.last = json['last']
+        self.buy = json['buy']
+        self.change = json['change']
+        self.sell = json['sell']
+        self.dayLow = json['dayLow']
+        self.close = json['close']
+        self.dayHigh = json['dayHigh']
+        self.open = json['open']
+        self.timestamp = json['timestamp']
+
+
+def read_file(file_path):
+    with codecs.open(file_path, 'r', 'UTF-8') as f:
+        lines = f.readlines()
+    return lines
+
+
+def check_3s_vol(line, val):
+    segs = line.split(',')
+    try:
+        vol = segs[5].split(':')[1]
+        if float(vol) > val:
+            print (line)
+    except:
+        print ('exception')
+
+
+def check_1min_vol(line, val):
+    segs = line.split(',')
+    vol = segs[8].split(':')[1]
+    if float(vol) > val:
+        return True
+    return False
+
+
+def check_ask_bid(line):
+    segs = line.split(',')
+    if len(segs) > 10:
+        ask_vol = float(segs[9].split(':')[1])
+        bid_vol = float(segs[10].split(':')[1])
+        ths_vol = float(segs[8].split(':')[1])
+        if ths_vol > 100000:
+            if ask_vol > 2 * bid_vol or bid_vol > 2 * ask_vol:
+                return True
+    return False
+
+
+def check_incr_rate(line):
+    segs = line.split(',')
+    if len(segs) > 10:
+        price_3s = float(segs[1].split(':')[1])
+        price_1m = float(segs[3].split(':')[1])
+        price_5m = float(segs[4].split(':')[1])
+        if price_3s > price_5m * 1.008:
+            return True
+
+
+def check_decr_rate(line):
+    segs = line.split(',')
+    if len(segs) > 10:
+        price_3s = float(segs[1].split(':')[1])
+        price_1m = float(segs[3].split(':')[1])
+        price_5m = float(segs[4].split(':')[1])
+        if price_3s < price_5m * 0.992:
+            return True
+
+
+def extract_time(line):
+    segs = line.split(',')
+    n = len(segs)
+    return segs[n - 1].strip()
+
+
+def query_24h_vol():
+    avg_vol = float(okSpot.ticker("eos_usdt")['ticker']['vol']) / 24 / 60
+    print(avg_vol)
+
+if __name__=='__main__':
+    query_24h_vol()
+    # path = '/Users/linchuanli/PycharmProjects/moneymaker'
+    # lines = read_file(path + '/eos_deals_20180912.txt')
+    # last_time = ''
+    # last_line = ''
+    # for line in lines:
+        # if last_line != '':
+        #     last_time = extract_time(last_line)
+        #     this_time = extract_time(line)
+        #     # print(time.mktime(time.strptime(last_time,'%Y-%m-%d %H:%M:%S')))
+        #     if time.mktime(time.strptime(this_time,'%Y-%m-%d %H:%M:%S')) - time.mktime(time.strptime(last_time, '%Y-%m-%d %H:%M:%S')) > 10:
+        #         print(last_line)
+        #         print(line + '\n')
+        # last_line = line
+
+        # if check_1min_vol(line, 80000):
+            # if check_ask_bid(line):
+        # if check_incr_rate(line) or check_decr_rate(line):
+        #     # print(line)
+        #     stime = extract_time(line)
+        #     if stime != last_time:
+        #         print(line)
+        #         last_time = stime
+        # check_buy_sell(line)
+        # check_ask_bid(line)
--- run.py
+++ run.py
@@ -1,90 +1,17 @@
-#!/usr/bin/python
-# -*- coding: UTF-8 -*-
+# !/usr/bin/python
+# -*-coding:UTF-8 -*-
 
-import websocket
-import codecs
 try:
     import thread
 except ImportError:
     import _thread as thread
-from collections import deque
-from trade import *
-import os
-import traceback
-
-
-class Coin:
-    def __init__(self, name, refer):
-        self.name = name
-        self.refer = refer
-
-    def gen_file_name(self):
-        file_path = os.getcwd()
-        transaction = file_path + '/' + self.name + '_transaction.txt'
-        deal = file_path + '/' + self.name + '_deals.txt'
-        return transaction, deal
-
-    def gen_full_name(self):
-        return self.name + "_" + self.refer
-
-
-class dealEntity:
-    def __init__(self, _id, _price, _amount, _time, _type):
-        self.id = _id
-        self.price = _price
-        self.amount = _amount
-        self.time = _time
-        self.type = _type
-
-    def detail(self):
-        if self.type == 'ask':
-            category = 'sell '
-        else:
-            category = 'buy  '
-        return str(self.time) + ': ' + category + str(self.amount) + '\t at price: ' + str(self.price)
-
-
-class indicator:
-    def __init__(self, interval):
-        self.interval = interval
-        self.vol = 0
-        self.avg_price = 0
-        self.price = 0
-        self.price_num = 0
-        self.bid_vol = 0
-        self.ask_vol = 0
-
-    def cal_avg_price(self):
-        if self.price_num != 0:
-            return round(float(self.price) / float(self.price_num), 4)
-        else:
-            return 0
-
-    def add_vol(self, deal_entity):
-        self.vol += deal_entity.amount
-        if deal_entity.type == 'ask':
-            self.ask_vol += deal_entity.amount
-        elif deal_entity.type == 'bid':
-            self.bid_vol += deal_entity.amount
-
-    def minus_vol(self, deal_entity):
-        self.vol -= deal_entity.amount
-        if deal_entity.type == 'ask':
-            self.ask_vol -= deal_entity.amount
-        elif deal_entity.type == 'bid':
-            self.bid_vol -= deal_entity.amount
-
-    def add_price(self, deal_entity):
-        self.price_num += 1
-        self.price += deal_entity.price
-
-    def minus_price(self, deal_entity):
-        self.price_num -= 1
-        self.price -= deal_entity.price
-
+from realtime import Coin,websocket,deque,indicator,dealEntity,time,codecs,traceback,cal_rate
+from time_utils import timestamp2string
+from trade import sell_more, sell_less, buyin_less_batch, buyin_more_batch, json
 
 # 默认币种
-coin = Coin("eos", "usdt")
+coin = Coin("etc", "usdt")
+time_type = "quarter"
 file_transaction, file_deal = coin.gen_file_name()
 
 deque_min = deque()
@@ -101,11 +28,9 @@ less = 0
 last_avg_price = 0
 buy_price = 0
 last_last_price = 0
-incr_5m_rate = 0.8
-incr_1m_rate = 0.3
-time_type = "this_week"
+incr_5m_rate = 1
+incr_1m_rate = 0.4
 write_lines = []
-avg_min_vol = 10000
 
 
 def connect():
@@ -128,20 +53,14 @@ def handle_deque(deq, entity, ts, ind):
     ind.add_vol(entity)
 
 
-def cal_rate(cur_price, last_price):
-    return round((cur_price - last_price) / last_price, 4) * 100
-
-
 def on_message(ws, message):
     if 'pong' in message or 'addChannel' in message:
-        print(message)
         return
     global latest_price, last_avg_price, buy_price, last_last_price, more, less, deque_3s, deque_10s, deque_min, \
         deque_5m, ind_3s, ind_10s, ind_1min, ind_5m, write_lines
     jmessage = json.loads(message)
     ts = time.time()
     now_time = timestamp2string(ts)
-
     jdata = jmessage[0]['data'][0]
     latest_price = float(jdata[1])
     deal_entity = dealEntity(jdata[0], float(jdata[1]), round(float(jdata[2]), 3), ts, jdata[4])
@@ -160,11 +79,10 @@ def on_message(ws, message):
     price_5m_change = cal_rate(avg_3s_price, avg_5m_price)
 
     if more == 1:
-        if avg_10s_price <= 1.004 * avg_5m_price or (ind_3s.vol > 20000 and ind_3s.ask_vol > 3 * ind_3s.bid_vol):
+        if avg_10s_price <= 1.004 * avg_5m_price or (ind_3s.vol > 2000 and ind_3s.ask_vol > 3 * ind_3s.bid_vol):
             # 按买一价出售
             if sell_more(coin.name, time_type):
-                sell_price = latest_price
-                info = u'发出卖出信号！！！卖出价格：' + str(sell_price) + u', 收益: ' + str(sell_price - buy_price) \
+                info = u'发出卖出信号！！！卖出价格：' + str(latest_price) + u', 收益: ' + str(latest_price - buy_price) \
                        + ', ' + now_time
                 print (info)
                 with codecs.open(file_transaction, 'a+', 'utf-8') as f:
@@ -173,8 +91,7 @@ def on_message(ws, message):
     elif less == 1:
         if avg_10s_price >= 0.996 * avg_5m_price:
             if sell_less(coin.name, time_type):
-                sell_price = latest_price
-                info = u'发出卖出信号！！！卖出价格：' + str(sell_price) + u', 收益: ' + str(buy_price - sell_price) \
+                info = u'发出卖出信号！！！卖出价格：' + str(latest_price) + u', 收益: ' + str(buy_price - latest_price) \
                        + ', ' + now_time
                 print (info)
                 with codecs.open(file_transaction, 'a+', 'utf-8') as f:
@@ -183,20 +100,22 @@ def on_message(ws, message):
     elif check_vol():
         if latest_price > avg_3s_price > avg_10s_price > last_avg_price > last_last_price \
                 and price_5m_change > incr_5m_rate and price_1m_change > incr_1m_rate \
-                and ind_1min.bid_vol > float(ind_1min.ask_vol) and ind_3s.bid_vol > float(2 * ind_3s.ask_vol):
+                and ind_1min.bid_vol > float(2 * ind_1min.ask_vol) and ind_3s.bid_vol > float(3 * ind_3s.ask_vol):
             if buyin_more_batch(coin.name, time_type, latest_price):
                 more = 1
                 buy_price = latest_price
                 info = u'发出做多信号！！！买入价格：' + str(buy_price) + u', ' + now_time
+                print (info)
                 with codecs.open(file_transaction, 'a+', 'utf-8') as f:
                     f.writelines(info + '\n')
 
         elif latest_price < avg_3s_price < avg_10s_price < last_avg_price < last_last_price \
                 and price_5m_change < -1 * incr_5m_rate and price_1m_change < -1 * incr_1m_rate \
-                and ind_1min.ask_vol > float(ind_1min.bid_vol) and ind_3s.ask_vol > float(2 * ind_3s.bid_vol):
+                and ind_1min.ask_vol > float(2 * ind_1min.bid_vol) and ind_3s.ask_vol > float(3 * ind_3s.bid_vol):
             if buyin_less_batch(coin.name, time_type, latest_price):
                 buy_price = latest_price
                 info = u'发出做空信号！！！买入价格：' + str(buy_price) + u', ' + now_time
+                print (info)
                 with codecs.open(file_transaction, 'a+', 'utf-8') as f:
                     f.writelines(info + '\n')
                 less = 1
@@ -207,8 +126,7 @@ def on_message(ws, message):
     price_info = deal_entity.type + u' now_price: %.4f, 3s_price: %.4f, 10s_price: %.4f, 1m_price: %.4f, ' \
                                     u'5min_price: %.4f' \
                  % (latest_price, avg_3s_price, avg_10s_price, avg_min_price, avg_5m_price)
-    vol_info = u'cur_vol: %.3f, 3s vol: %.3f, 10s vol: %.3f, 1min vol: %.3f, ask_vol: %.3f, bid_vol: %.3f, 3s_ask_vol: ' \
-               u'%.3f, 3s_bid_vol: %.3f' \
+    vol_info = u'cur_vol: %.3f, 3s vol: %.3f, 10s vol: %.3f, 1min vol: %.3f, ask_vol: %.3f, bid_vol: %.3f, 3s_ask_vol: %.3f, 3s_bid_vol: %.3f' \
                % (deal_entity.amount, ind_3s.vol, ind_10s.vol, ind_1min.vol, ind_1min.ask_vol, ind_1min.bid_vol, ind_3s.ask_vol, ind_3s.bid_vol)
     rate_info = u'10s_rate: %.2f%%, 1min_rate: %.2f%%, 5min_rate: %.2f%%' \
                 % (price_10s_change, price_1m_change, price_5m_change)
@@ -222,16 +140,8 @@ def on_message(ws, message):
     print(price_info + '\r\n' + vol_info + '\r\n' + rate_info + u', ' + now_time)
 
 
-def query_24h_vol():
-    global avg_min_vol
-    while 1:
-        avg_min_vol = float(okSpot.ticker("eos_usdt")['ticker']['vol']) / 24 / 60
-        print('1min avg_vol: %.3f' % avg_min_vol)
-        time.sleep(60)
-
-
 def check_vol():
-    if ind_1min.vol > 5 * avg_min_vol and ind_3s.vol > avg_min_vol:
+    if ind_1min.vol > 16000 and ind_3s.vol > 2000:
         return True
     else:
         return False
@@ -248,22 +158,20 @@ def on_close(ws):
 
 def on_open(ws):
     def run(*args):
-        ws.send("{'event':'addChannel','channel':'ok_sub_spot_eos_usdt_deals'}")
+        ws.send("{'event':'addChannel','channel':'ok_sub_spot_%s_deals'}" % coin.gen_full_name())
         print("thread starting...")
 
     thread.start_new_thread(run, ())
 
 
-if __name__ == "__main__":
-    # websocket.enableTrace(True)
+if __name__ == '__main__':
     ws = websocket.WebSocketApp("wss://real.okex.com:10441/websocket",
                                 on_message=on_message,
                                 on_error=on_error,
                                 on_close=on_close)
     ws.on_open = on_open
     while True:
-        thread.start_new_thread(query_24h_vol, ())
-        ws.run_forever(ping_interval=10, ping_timeout=8)
+        ws.run_forever(ping_interval=20, ping_timeout=10)
         print("write left lines into file...")
         with codecs.open(file_deal, 'a+', 'UTF-8') as f:
             f.writelines(write_lines)
