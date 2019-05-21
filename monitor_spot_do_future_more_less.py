39a8a7e47f688d4a8b8e7279e9de94e2220d15ab
lilinchuan <lilinchuan@kuaishou.com>
2019-05-21 11:02:42 +0800

    change condition

--- monitor_spot_do_future_more_less.py
+++ monitor_spot_do_future_more_less.py
@@ -57,17 +57,12 @@ def handle_deque(deq, entity, ts, ind):
 
 
 def check_do_future_less(price_3m_change, price_1m_change, price_10s_change):
-    if ind_1min.vol > 200000 and ind_1min.ask_vol > 1.8 * ind_1min.bid_vol \
-            and ind_3m.vol > 400000 and ind_3m.ask_vol > 1.5 * ind_3m.bid_vol and -1.2 < price_1m_change \
+    if ind_1min.vol > 300000 and ind_1min.ask_vol > 1.5 * ind_1min.bid_vol \
+            and ind_3m.vol > 400000 and ind_3m.ask_vol > 1.3 * ind_3m.bid_vol and -1.2 < price_1m_change \
             and price_3m_change < price_1m_change < -0.3 and price_10s_change <= -0.05 and new_macd < 0:
         return True
     elif ind_1min.vol > 200000 and ind_1min.ask_vol > 2 * ind_1min.bid_vol \
-            and ind_3m.vol > 250000 and ind_3m.ask_vol > 1.5 * ind_3m.bid_vol \
-            and price_3m_change < price_1m_change < -0.2 and price_10s_change <= -0.05 \
-            and new_macd < 0:
-        return True
-    elif ind_1min.vol > 150000 and ind_1min.ask_vol > 2 * ind_1min.bid_vol \
-            and ind_3m.vol > 200000 and ind_3m.ask_vol > 1.5 * ind_3m.bid_vol \
+            and ind_3m.vol > 300000 and ind_3m.ask_vol > 1.5 * ind_3m.bid_vol \
             and price_3m_change < price_1m_change < -0.2 and price_10s_change <= -0.05 \
             and new_macd < 0:
         return True
@@ -75,14 +70,14 @@ def check_do_future_less(price_3m_change, price_1m_change, price_10s_change):
 
 
 def check_do_future_more(price_3m_change, price_1m_change, price_10s_change):
-    if ind_1min.vol > 200000 and ind_1min.ask_vol * 2 < ind_1min.bid_vol \
-            and ind_3m.vol > 250000 and ind_3m.ask_vol * 1.3 < ind_3m.bid_vol \
-            and price_3m_change > price_1m_change > 0.2 and price_10s_change >= 0.05 \
+    if ind_1min.vol > 300000 and ind_1min.ask_vol * 2 < ind_1min.bid_vol \
+            and ind_3m.vol > 400000 and ind_3m.ask_vol * 1.3 < ind_3m.bid_vol \
+            and price_3m_change > price_1m_change > 0.3 and price_10s_change >= 0.1 \
             and new_macd > 0:
         return True
-    elif ind_1min.vol > 150000 and ind_1min.ask_vol * 2 < ind_1min.bid_vol \
-            and ind_3m.vol > 200000 and ind_3m.ask_vol * 1.5 < ind_3m.bid_vol \
-            and price_3m_change > price_1m_change > 0.2 and price_10s_change >= 0.05 \
+    elif ind_1min.vol > 200000 and ind_1min.ask_vol * 3 < ind_1min.bid_vol \
+            and ind_3m.vol > 300000 and ind_3m.ask_vol * 1.5 < ind_3m.bid_vol \
+            and price_3m_change > price_1m_change > 0.3 and price_10s_change >= 0.1 \
             and new_macd > 0:
         return True
     return False
